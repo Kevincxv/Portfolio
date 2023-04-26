a88bba42a5edde518de814f26743d8620c99574b
Kevin <Kevincxv@outlook.com>
2023-04-26 15:01:50 -0400

    Update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,9 +9,8 @@ const Footer = () => {
             <Layout className='py-8 flex items-center justify-between lg:flex-col lg:py-6'>
                 <span>{new Date().getFullYear()} &copy; All Rights Reserved</span>
                 <div className='flex items-center lg:py-2'>
-                    Build with <span className='text-dark dark:text-primaryDark text-2xl px-1'>&#9825;</span>
                 
-                by&nbsp;<p>Kevin Camacho</p>
+                By&nbsp;<p>Kevin Camacho</p>
                 </div>
                 <Link href="mailto:kevincxv@outlook.com" target={"_blank"} >Say Hello</Link>
                 </Layout>
